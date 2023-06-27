fc82afbc72e080fda53e5071274f37020cebbc5c
Abhishek Verma <ak8888780@gmail.com>
2023-06-27 16:29:24 +0530

    DAY 1 data 27th june 2023

---
+++ Day1/exercise.py
@@ -0,0 +1,32 @@
+# 1
+a = 1
+b = 3
+print(a+b) #addition
+print(a-b) #subtraction
+print(a*b) #multiply
+print(a%b) #modulus
+print(a/b) #division
+print(a**b) #exponantial
+print(a//b) #flooor division operator
+
+# 2
+name = "helloabhii"
+family = "kumhar"
+country = "India"
+me = "i am doing 30 days of challange currently"
+print(name,family,country,me)
+
+# 3
+#check the data type of following
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['helloabhii',21,True]))
+print(type('Abhishek'))
+print(type('Kumhar'))
+print(type('India'))
+
+#4
+z= "abhii"
+print(z) #it is string data type
