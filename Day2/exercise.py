fb00b99adee2e912b1a3d67d3200e35ca17d5af9
Abhishek Verma <ak8888780@gmail.com>
2023-06-29 23:01:50 +0530

    Day 2 29th June 2023 Thrusday

---
+++ Day2/exercise.py
@@ -0,0 +1,52 @@
+#exercise 1
+first_name =  "Abhishek"
+last_name = "Verma"
+full_name = "Abhishek Verma"
+Country = "India"
+City = "Hamirpur"
+age = 21
+year = 2023
+multiple = [21,"Hiachal pradesh", "Open source"]
+print(multiple)
+
+#exercise 2
+print(type(first_name))
+print(type(last_name))
+print(type(age))
+print(type(year))
+print(type(multiple))
+
+print(len(first_name))
+
+num_one = 5 
+num_two = 4
+total = (num_one + num_two)
+print(total)
+
+diff = (num_two - num_one)
+print(diff)
+
+product = (num_one * num_two)
+print(product)
+
+division = (num_one // num_two)
+print(division)
+
+var_rem = (num_one % num_two) #modulus
+print(var_rem)
+
+var_exp = (num_one ** num_two)
+print(var_exp) #exponential
+
+floor_div = (num_one // num_two)
+print(floor_div)
+
+
+
+
+#getting input from user
+name = input("What is Your Name :")
+country = input("Where you from :")
+age = input("What's your age :")
+print("so your name is",name ,"and you from ",country,"and your age is ",age)
+
